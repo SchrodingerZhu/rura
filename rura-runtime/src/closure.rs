62ad3da526607bc584606f94feb7d640de440fab
Schrodinger ZHU Yifan <yifanzhu@rochester.edu>
2024-02-08 12:18:18 -0500

    feat: erase wrapper in code

--- rura-runtime/src/closure.rs
+++ rura-runtime/src/closure.rs
@@ -7,6 +7,7 @@ pub trait PartialParams {
     type Full;
     type Progress: PartialParams<Full = Self::Full>;
     fn apply(&mut self, next: Self::Next);
+    unsafe fn transmute(self) -> Self::Full;
 }
 
 #[repr(transparent)]
@@ -26,46 +27,62 @@ impl PartialParams for () {
     type Progress = ();
     type Full = ();
     fn apply(&mut self, _: Self::Next) {}
+    unsafe fn transmute(self) -> Self::Full {}
 }
 
 impl<T> PartialParams for (Ready<T>,) {
     type Next = ();
     type Progress = (Ready<T>,);
-    type Full = (Ready<T>,);
+    type Full = (T,);
     fn apply(&mut self, _: Self::Next) {}
+    unsafe fn transmute(self) -> Self::Full {
+        ((self.0).0,)
+    }
 }
 
 impl<T> PartialParams for (Hole<T>,) {
     type Next = T;
     type Progress = (Ready<T>,);
-    type Full = (Ready<T>,);
+    type Full = (T,);
     fn apply(&mut self, next: Self::Next) {
         self.0 .0.write(next);
+    }
+    unsafe fn transmute(self) -> Self::Full {
+        ((self.0).0.assume_init(),)
     }
 }
 
 impl<A, B> PartialParams for (Ready<A>, Ready<B>) {
     type Next = ();
     type Progress = (Ready<A>, Ready<B>);
-    type Full = (Ready<A>, Ready<B>);
+    type Full = (A, B);
     fn apply(&mut self, _: Self::Next) {}
+    unsafe fn transmute(self) -> Self::Full {
+        ((self.0).0, (self.1).0)
+    }
 }
 
 impl<A, B> PartialParams for (Ready<A>, Hole<B>) {
     type Next = B;
     type Progress = (Ready<A>, Ready<B>);
-    type Full = (Ready<A>, Ready<B>);
+    type Full = (A, B);
     fn apply(&mut self, next: Self::Next) {
         self.1 .0.write(next);
+    }
+    unsafe fn transmute(self) -> Self::Full {
+        ((self.0).0, (self.1).0.assume_init())
     }
 }
 
 impl<A, B> PartialParams for (Hole<A>, Hole<B>) {
     type Next = A;
     type Progress = (Ready<A>, Hole<B>);
-    type Full = (Ready<A>, Ready<B>);
+    type Full = (A, B);
     fn apply(&mut self, next: Self::Next) {
         self.0 .0.write(next);
+    }
+    unsafe fn transmute(self) -> Self::Full {
+        ((self.0).0.assume_init(), (self.1).0.assume_init())
     }
 }
 
@@ -86,9 +103,9 @@ impl<P: PartialParams + Clone, R> Clone for Thunk<P, R> {
 impl<P: PartialParams, R> Thunk<P, R> {
     pub fn eval(self) -> R
     where
-        P: PartialParams<Full = P>,
+        P: PartialParams<Progress = P>,
     {
-        (self.code)(self.params)
+        (self.code)(unsafe { self.params.transmute() })
     }
     pub fn apply(&mut self, x: P::Next) {
         self.params.apply(x);
@@ -113,7 +130,7 @@ impl<P: PartialParams + Clone, R> Closure<P, R> {
     }
     pub fn eval(self) -> R
     where
-        P: PartialParams<Full = P>,
+        P: PartialParams<Progress = P>,
     {
         let f = Rc::unwrap_or_clone(self.0);
         f.eval()
@@ -127,7 +144,7 @@ mod test {
     #[test]
     fn test_closure() {
         let f = Closure(Rc::new(Thunk {
-            code: |(Ready(a), Ready(b)): (Ready<i32>, Ready<i32>)| a + b,
+            code: |(a, b)| a + b,
             params: (Hole(MaybeUninit::uninit()), Hole(MaybeUninit::uninit())),
         }));
         assert_eq!(f.apply(1).apply(2).eval(), 3);

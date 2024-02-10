f94163ebe810a1fe7b4190fe640fcb131a888ee3
Schrodinger ZHU Yifan <yifanzhu@rochester.edu>
2024-02-10 01:02:28 -0500

    feat: allow erased closure to be used as fixed-arity closure

--- rura-runtime/src/closure.rs
+++ rura-runtime/src/closure.rs
@@ -48,7 +48,9 @@ impl<P: PartialParams + Clone, R> Clone for Thunk<P, R> {
     }
 }
 pub trait BoxedClosure<P: Params, R> {
-    fn apply(self: Rc<Self>, param: P::Head) -> Rc<dyn BoxedClosure<P::Tail, R>>;
+    fn apply(self: Rc<Self>, param: P::Head) -> Rc<dyn BoxedClosure<P::Tail, R>>
+    where
+        P::Head: Applicable;
     fn eval(self: Rc<Self>) -> R
     where
         P: Params<Head = ()>;
@@ -108,7 +110,10 @@ impl<P, R> Clone for Closure<P, R> {
 }
 
 impl<P: Params, R> Closure<P, R> {
-    pub fn apply(self, param: P::Head) -> Closure<P::Tail, R> {
+    pub fn apply(self, param: P::Head) -> Closure<P::Tail, R>
+    where
+        P::Head: Applicable,
+    {
         Closure(self.0.apply(param))
     }
 
@@ -121,7 +126,10 @@ impl<P: Params, R> Closure<P, R> {
 }
 
 #[cfg(feature = "nightly")]
-impl<P: Params, R> FnOnce<(P::Head,)> for Closure<P, R> {
+impl<P: Params, R> FnOnce<(P::Head,)> for Closure<P, R>
+where
+    P::Head: Applicable,
+{
     type Output = Closure<P::Tail, R>;
     extern "rust-call" fn call_once(self, x: (P::Head,)) -> Self::Output {
         self.apply(x.0)
@@ -149,6 +157,9 @@ pub union ScalarPack {
     f32: f32,
     f64: f64,
     usize: usize,
+    char: char,
+    bool: bool,
+    unit: (),
 }
 
 #[derive(Clone)]
@@ -220,6 +231,133 @@ impl<R> ErasedThunk<R> {
     pub fn eval(self: Rc<Self>) -> R {
         let thunk = Rc::unwrap_or_clone(self);
         (thunk.code)(thunk.scalar, thunk.boxed)
+    }
+}
+
+pub trait Applicable {
+    fn apply<R>(self, closure: Rc<ErasedThunk<R>>) -> Rc<ErasedThunk<R>>;
+}
+
+impl Applicable for i8 {
+    fn apply<R>(self, closure: Rc<ErasedThunk<R>>) -> Rc<ErasedThunk<R>> {
+        closure.apply_scalar(ScalarPack { i8: self })
+    }
+}
+
+impl Applicable for i16 {
+    fn apply<R>(self, closure: Rc<ErasedThunk<R>>) -> Rc<ErasedThunk<R>> {
+        closure.apply_scalar(ScalarPack { i16: self })
+    }
+}
+
+impl Applicable for i32 {
+    fn apply<R>(self, closure: Rc<ErasedThunk<R>>) -> Rc<ErasedThunk<R>> {
+        closure.apply_scalar(ScalarPack { i32: self })
+    }
+}
+
+impl Applicable for i64 {
+    fn apply<R>(self, closure: Rc<ErasedThunk<R>>) -> Rc<ErasedThunk<R>> {
+        closure.apply_scalar(ScalarPack { i64: self })
+    }
+}
+
+impl Applicable for u8 {
+    fn apply<R>(self, closure: Rc<ErasedThunk<R>>) -> Rc<ErasedThunk<R>> {
+        closure.apply_scalar(ScalarPack { u8: self })
+    }
+}
+
+impl Applicable for u16 {
+    fn apply<R>(self, closure: Rc<ErasedThunk<R>>) -> Rc<ErasedThunk<R>> {
+        closure.apply_scalar(ScalarPack { u16: self })
+    }
+}
+
+impl Applicable for u32 {
+    fn apply<R>(self, closure: Rc<ErasedThunk<R>>) -> Rc<ErasedThunk<R>> {
+        closure.apply_scalar(ScalarPack { u32: self })
+    }
+}
+
+impl Applicable for u64 {
+    fn apply<R>(self, closure: Rc<ErasedThunk<R>>) -> Rc<ErasedThunk<R>> {
+        closure.apply_scalar(ScalarPack { u64: self })
+    }
+}
+
+impl Applicable for f32 {
+    fn apply<R>(self, closure: Rc<ErasedThunk<R>>) -> Rc<ErasedThunk<R>> {
+        closure.apply_scalar(ScalarPack { f32: self })
+    }
+}
+
+impl Applicable for f64 {
+    fn apply<R>(self, closure: Rc<ErasedThunk<R>>) -> Rc<ErasedThunk<R>> {
+        closure.apply_scalar(ScalarPack { f64: self })
+    }
+}
+
+impl Applicable for usize {
+    fn apply<R>(self, closure: Rc<ErasedThunk<R>>) -> Rc<ErasedThunk<R>> {
+        closure.apply_scalar(ScalarPack { usize: self })
+    }
+}
+
+impl Applicable for char {
+    fn apply<R>(self, closure: Rc<ErasedThunk<R>>) -> Rc<ErasedThunk<R>> {
+        closure.apply_scalar(ScalarPack { char: self })
+    }
+}
+
+impl Applicable for bool {
+    fn apply<R>(self, closure: Rc<ErasedThunk<R>>) -> Rc<ErasedThunk<R>> {
+        closure.apply_scalar(ScalarPack { bool: self })
+    }
+}
+
+impl Applicable for () {
+    fn apply<R>(self, closure: Rc<ErasedThunk<R>>) -> Rc<ErasedThunk<R>> {
+        closure.apply_scalar(ScalarPack { unit: self })
+    }
+}
+
+impl Applicable for i128 {
+    fn apply<R>(self, closure: Rc<ErasedThunk<R>>) -> Rc<ErasedThunk<R>> {
+        closure.apply_i128(self)
+    }
+}
+
+impl Applicable for u128 {
+    fn apply<R>(self, closure: Rc<ErasedThunk<R>>) -> Rc<ErasedThunk<R>> {
+        closure.apply_u128(self)
+    }
+}
+
+impl<T: 'static> Applicable for Rc<T> {
+    fn apply<R>(self, closure: Rc<ErasedThunk<R>>) -> Rc<ErasedThunk<R>> {
+        closure.apply_object(self)
+    }
+}
+
+impl<P: Params, R> Applicable for Closure<P, R> {
+    fn apply<T>(self, closure: Rc<ErasedThunk<T>>) -> Rc<ErasedThunk<T>> {
+        closure.apply_cloure(self)
+    }
+}
+
+impl<P: Params, R: 'static> BoxedClosure<P, R> for ErasedThunk<R> {
+    fn apply(self: Rc<Self>, param: P::Head) -> Rc<dyn BoxedClosure<P::Tail, R>>
+    where
+        P::Head: Applicable,
+    {
+        Applicable::apply(param, self)
+    }
+    fn eval(self: Rc<Self>) -> R
+    where
+        P: Params<Head = ()>,
+    {
+        self.eval()
     }
 }
 
@@ -305,5 +443,25 @@ mod test {
         let h = g.clone();
         assert_eq!(g.apply(2).apply(2).apply(3).eval(), 13);
         assert_eq!(h.apply(4).apply(5).apply(6).eval(), 21);
+    }
+
+    #[test]
+    fn erased_thunk_as_closure() {
+        let f = ErasedThunk::new(|scalar, objects| {
+            let a = unsafe { scalar[0].i32 };
+            let b = unsafe { scalar[1].i32 };
+            let mut objects = objects.into_iter();
+            let Some(BoxedPack::Object(obj)) = objects.next() else {
+                unreachable!()
+            };
+            let obj: Closure<(i32, i32), i32> = unsafe { core::mem::transmute(obj) };
+            obj.apply(a).apply(b).eval()
+        });
+        let f: Rc<dyn BoxedClosure<(i32, i32, Closure<(i32, i32), i32>), i32>> = f.clone();
+        let g = f.apply(1).apply(2).apply(Closure(Rc::new(Thunk {
+            code: |(x, y)| x + y,
+            params: (Hole(MaybeUninit::uninit()), Hole(MaybeUninit::uninit())),
+        })));
+        assert_eq!(g.eval(), 3);
     }
 }

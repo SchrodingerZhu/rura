f5d210bdd48fe7cc978eaa03318d4ee5a4f18119
Schrodinger ZHU Yifan <yifanzhu@rochester.edu>
2024-02-12 16:25:41 -0500

    fix shape analysis

--- rura-core/src/lib.rs
+++ rura-core/src/lib.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 pub mod lir;
 
 #[repr(transparent)]
@@ -41,15 +43,9 @@ pub struct Constructor {
 }
 
 #[derive(Clone, Debug, PartialEq, Eq, Hash)]
-pub enum TypeParam {
-    Resolved(Box<RuraType>),
-    Pending(Ident),
-}
-
-#[derive(Clone, Debug, PartialEq, Eq, Hash)]
 pub struct InductiveType {
     pub qualified_name: QualifiedName,
-    pub type_params: Box<[TypeParam]>,
+    pub type_params: Box<[Ident]>,
     pub constructors: Box<[Constructor]>,
 }
 
@@ -69,6 +65,7 @@ pub enum RuraType {
     Tuple(Box<[RuraType]>),
     TypeVar(Ident),
     TypeRef(QualifiedName),
+    ResolvedInductive(Box<InductiveType>, Box<[RuraType]>),
 }
 
 #[derive(Clone, Debug, PartialEq, Eq, Hash, Ord, PartialOrd)]
@@ -84,55 +81,96 @@ pub enum Shape {
     TypeVar(Ident),
 }
 
+#[derive(Debug, thiserror::Error)]
+pub enum ShapeError {
+    #[error("Inductive type has unsolved type parameters")]
+    UnresolvedType,
+    #[error("Bottom type does not have a shape")]
+    BottomType,
+}
+
+fn get_shape_inductive<'a, 'b: 'a>(
+    inductive: &'b InductiveType,
+    context: &'a mut HashMap<&'b Ident, Shape>,
+) -> Result<Shape, ShapeError> {
+    let mut shapes = inductive
+        .constructors
+        .iter()
+        .map(|c| {
+            c.args
+                .iter()
+                .map(|t| get_shape(t, context))
+                .collect::<Result<Vec<_>, _>>()
+        })
+        .map(|x| x.map(Vec::into_boxed_slice))
+        .map(|x| x.map(Shape::Composite))
+        .collect::<Result<Vec<_>, _>>()?;
+    shapes.sort_unstable();
+    Ok(Shape::Alternative(shapes.into_boxed_slice()))
+}
+
+fn get_shape<'a, 'b: 'a>(
+    rura_type: &'b RuraType,
+    context: &'a mut HashMap<&'b Ident, Shape>,
+) -> Result<Shape, ShapeError> {
+    match rura_type {
+        RuraType::Scalar(scalar) => Ok(Shape::Scalar(*scalar)),
+        RuraType::Unit => Ok(Shape::Unit),
+        RuraType::Bottom => Err(ShapeError::BottomType),
+        RuraType::Inductive(inductive) => {
+            if !inductive.type_params.is_empty() {
+                return Err(ShapeError::UnresolvedType);
+            }
+            get_shape_inductive(inductive.as_ref(), context)
+        }
+        RuraType::Closure(x) => {
+            if x.args.len() > 16 {
+                Ok(Shape::ErasedClosure)
+            } else {
+                Ok(Shape::Closure)
+            }
+        }
+        RuraType::Tuple(tuple) => {
+            let shapes = tuple
+                .iter()
+                .map(|t| Shape::try_from(t))
+                .collect::<Result<Vec<_>, _>>()?;
+            Ok(Shape::Tuple(shapes.into_boxed_slice()))
+        }
+        RuraType::TypeVar(ident) => {
+            if let Some(t) = context.get(ident).cloned() {
+                Ok(t)
+            } else {
+                Ok(Shape::TypeVar(ident.clone()))
+            }
+        }
+        RuraType::TypeRef(_) => Ok(Shape::Object),
+        RuraType::ResolvedInductive(inductive, args) => {
+            let mut changelog = Vec::new();
+            for (param, arg) in inductive.type_params.iter().zip(args.iter()) {
+                let shape = get_shape(arg, context)?;
+                changelog.push((param, context.insert(param, shape)));
+            }
+            let shape = get_shape_inductive(inductive.as_ref(), context);
+            for (param, old) in changelog {
+                if let Some(old) = old {
+                    context.insert(param, old);
+                } else {
+                    context.remove(param);
+                }
+            }
+            shape
+        }
+    }
+}
+
 impl TryFrom<&'_ RuraType> for Shape {
     // TODO: Implement this
-    type Error = ();
+    type Error = ShapeError;
 
     fn try_from(value: &'_ RuraType) -> Result<Self, Self::Error> {
-        match value {
-            RuraType::Scalar(scalar) => Ok(Shape::Scalar(*scalar)),
-            RuraType::Unit => Ok(Shape::Unit),
-            RuraType::Bottom => Err(()),
-            RuraType::Inductive(inductive) => {
-                if inductive
-                    .type_params
-                    .iter()
-                    .any(|t| matches!(t, TypeParam::Pending(_)))
-                {
-                    return Err(());
-                }
-                let mut shapes = inductive
-                    .constructors
-                    .iter()
-                    .map(|c| {
-                        c.args
-                            .iter()
-                            .map(|t| Shape::try_from(t))
-                            .collect::<Result<Vec<_>, _>>()
-                    })
-                    .map(|x| x.map(Vec::into_boxed_slice))
-                    .map(|x| x.map(Shape::Composite))
-                    .collect::<Result<Vec<_>, _>>()?;
-                shapes.sort_unstable();
-                Ok(Shape::Alternative(shapes.into_boxed_slice()))
-            }
-            RuraType::Closure(x) => {
-                if x.args.len() > 16 {
-                    Ok(Shape::ErasedClosure)
-                } else {
-                    Ok(Shape::Closure)
-                }
-            }
-            RuraType::Tuple(tuple) => {
-                let shapes = tuple
-                    .iter()
-                    .map(|t| Shape::try_from(t))
-                    .collect::<Result<Vec<_>, _>>()?;
-                Ok(Shape::Tuple(shapes.into_boxed_slice()))
-            }
-            RuraType::TypeVar(ident) => Ok(Shape::TypeVar(ident.clone())),
-            RuraType::TypeRef(_) => Ok(Shape::Object),
-        }
+        let mut context = HashMap::new();
+        get_shape(value, &mut context)
     }
 }
 

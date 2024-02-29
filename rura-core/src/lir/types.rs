2c4b8178de42ac6d954fe7522c98e5503adfd4a2
Schrodinger ZHU Yifan <yifanzhu@rochester.edu>
2024-02-29 10:10:45 -0500

    [lir] support unified comparison between types

--- rura-core/src/lir/types.rs
+++ rura-core/src/lir/types.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::{fmt::Display, fmt::Formatter};
 
 use crate::lir::{Ident, QualifiedName};
@@ -100,5 +101,54 @@ impl From<(Box<[Self]>, Self)> for LirType {
     fn from(f: (Box<[Self]>, Self)) -> Self {
         let (params, ret) = f;
         Self::Closure(params, Box::new(ret))
+    }
+}
+
+// unify simple type arguments and compare the type equality
+pub fn unified_compare<'a>(
+    unifier: &'_ HashMap<&'a Ident, &'a LirType>,
+    unified: &'a LirType,
+    target: &'a LirType,
+) -> bool {
+    match (unified, target) {
+        (LirType::Primitive(a), LirType::Primitive(b)) => a == b,
+        (LirType::Unit, LirType::Unit) => true,
+        (LirType::Bottom, LirType::Bottom) => true,
+        (LirType::Closure(a, b), LirType::Closure(c, d)) => {
+            a.len() == c.len()
+                && a.iter()
+                    .zip(c.iter())
+                    .all(|(x, y)| unified_compare(unifier, x, y))
+                && unified_compare(unifier, b, d)
+        }
+        (LirType::Object(a, b), LirType::Object(c, d)) => {
+            a == c
+                && b.len() == d.len()
+                && b.iter()
+                    .zip(d.iter())
+                    .all(|(x, y)| unified_compare(unifier, x, y))
+        }
+        (LirType::Tuple(a), LirType::Tuple(b)) => {
+            a.len() == b.len()
+                && a.iter()
+                    .zip(b.iter())
+                    .all(|(x, y)| unified_compare(unifier, x, y))
+        }
+        (LirType::TypeVar(a), b) => {
+            if let TypeVar::Plain(a) = a {
+                if let Some(a) = unifier.get(a) {
+                    return unified_compare(unifier, a, b);
+                }
+            }
+            if let LirType::TypeVar(b) = b {
+                return a == b;
+            }
+            false
+        }
+        (LirType::Hole(a), LirType::Hole(b)) => unified_compare(unifier, a, b),
+        (LirType::Ref(a), LirType::Ref(b)) => unified_compare(unifier, a, b),
+        (LirType::Token(a), LirType::Token(b)) => unified_compare(unifier, a, b),
+        (LirType::Unique(a), LirType::Unique(b)) => unified_compare(unifier, a, b),
+        _ => false,
     }
 }
